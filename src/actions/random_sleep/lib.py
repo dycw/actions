cf74766b79c5cc20e63a3a5ab906df5d3548a6be
Derek Wan <d.wan@icloud.com>
2025-12-19 10:11:34 +0900

    2025-12-19 10:11:34 (Fri)  > DW-Mac  > derekwan

---
+++ src/actions/random_sleep/lib.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from random import choice
+from time import sleep
+
+from utilities.whenever import get_now
+from whenever import TimeDelta, ZonedDateTime
+
+from actions import __version__
+from actions.logging import LOGGER
+from actions.random_sleep.settings import RANDOM_SLEEP_SETTINGS
+
+
+def random_sleep(
+    *,
+    min_: int = RANDOM_SLEEP_SETTINGS.min,
+    max_: int = RANDOM_SLEEP_SETTINGS.max,
+    step: int = RANDOM_SLEEP_SETTINGS.step,
+    log_freq: int = RANDOM_SLEEP_SETTINGS.log_freq,
+) -> None:
+    LOGGER.info(
+        """\
+Running %r (version %s) with settings:
+ - min_     = %s
+ - max_     = %s
+ - step     = %s
+ - log_freq = %s
+""",
+        random_sleep.__name__,
+        __version__,
+        min_,
+        max_,
+        step,
+        log_freq,
+    )
+    start = get_now()
+    delta = TimeDelta(seconds=choice(range(min_, max_, step)))
+    LOGGER.info("Sleeping for %s...", delta)
+    end = start + delta
+    while (now := get_now()) < end:
+        _intermediate(start, now, end, log_freq=log_freq)
+    LOGGER.info("Finished sleeping for %s", delta)
+
+
+def _intermediate(
+    start: ZonedDateTime,
+    now: ZonedDateTime,
+    end: ZonedDateTime,
+    /,
+    *,
+    log_freq: int = RANDOM_SLEEP_SETTINGS.log_freq,
+) -> None:
+    elapsed = now - start
+    remaining = end - now
+    this_sleep = min(remaining, TimeDelta(seconds=log_freq))
+    LOGGER.info(
+        "Sleeping for %s... (elapsed = %s, remaining = %s)",
+        this_sleep,
+        elapsed,
+        remaining,
+    )
+    sleep(round(this_sleep.in_seconds()))
+
+
+__all__ = ["random_sleep"]
