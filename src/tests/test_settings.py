8f81d53ef81ffde3ed0192324190c8ed1deecbd4
Derek Wan <d.wan@icloud.com>
2025-12-23 16:01:47 +0900

    2025-12-23 16:01:47 (Tue)  > DW-Mac  > derekwan

--- src/tests/test_settings.py
+++ src/tests/test_settings.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
-from typed_settings import EnvLoader, load_settings
+from typed_settings import load_settings
 from utilities.os import temp_environ
 
 from actions.settings import CommonSettings
+from actions.utilities import LOADER
 
 
 class TestCommonSettings:
     def test_empty_strs(self) -> None:
         with temp_environ(TOKEN=""):
-            settings = load_settings(CommonSettings, [EnvLoader("")])
+            settings = load_settings(CommonSettings, [LOADER])
         assert settings.token is None
