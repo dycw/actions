ecfb849200aab3264b76012a50c8c446762248b3
Derek Wan <d.wan@icloud.com>
2025-12-23 15:48:39 +0900

    2025-12-23 15:48:39 (Tue)  > DW-Mac  > derekwan

--- src/actions/publish/settings.py
+++ src/actions/publish/settings.py
@@ -1,8 +1,30 @@
 from __future__ import annotations
 
-from typed_settings import Secret, load_settings, option, secret, settings
+from typing import TYPE_CHECKING, assert_never
 
+from typed_settings import Secret, load_settings, option, secret, settings
+from utilities.logging import basic_config
+
+from actions.logging import LOGGER
 from actions.utilities import LOADER, empty_str_to_none
+
+if TYPE_CHECKING:
+    from actions.types import SecretLike
+
+basic_config(obj=LOGGER)
+
+
+def empty_str_to_none2(value: SecretLike, /) -> Secret[str] | None:
+    return empty_str_to_none(value)
+    match value:
+        case Secret():
+            return value
+        case str():
+            return None if value == "" else Secret(value)
+        case None:
+            return None
+        case never:
+            assert_never(never)
 
 
 @settings
@@ -11,7 +33,7 @@ class PublishSettings:
         default=None, converter=empty_str_to_none, help="The username of the upload"
     )
     password: Secret[str] | None = secret(
-        default=None, converter=empty_str_to_none, help="The password for the upload"
+        default=None, converter=empty_str_to_none2, help="The password for the upload"
     )
     publish_url: str | None = option(
         default=None, converter=empty_str_to_none, help="The URL of the upload endpoint"
--- src/tests/test_settings.py
+++ src/tests/test_settings.py
@@ -1,9 +1,21 @@
 from __future__ import annotations
 
-from typed_settings import EnvLoader, load_settings
+from click import command, echo
+from click.testing import CliRunner
+from pytest import mark
+from typed_settings import (
+    EnvLoader,
+    Secret,
+    click_options,
+    load_settings,
+    option,
+    settings,
+)
 from utilities.os import temp_environ
 
+from actions.publish.settings import empty_str_to_none2
 from actions.settings import CommonSettings
+from actions.utilities import LOADER
 
 
 class TestCommonSettings:
@@ -11,3 +23,57 @@ class TestCommonSettings:
         with temp_environ(TOKEN=""):
             settings = load_settings(CommonSettings, [EnvLoader("")])
         assert settings.token is None
+
+
+@mark.only
+class TestEmptyStrToNone:
+    def test_value_missing(self) -> None:
+        @settings
+        class Settings:
+            key: Secret[str] | None = option(default=None, converter=empty_str_to_none2)
+
+        @command()
+        @click_options(Settings, [LOADER], show_envvars_in_help=True)
+        def cli(settings: Settings, /) -> None:
+            if (value := settings.key) is None:
+                echo("key = None")
+            else:
+                echo(f"key = {value.get_secret_value()}")
+
+        result = CliRunner().invoke(cli)
+        assert result.exit_code == 0
+        assert result.stdout == "key = None\n"
+
+    def test_value_via_cli(self) -> None:
+        @settings
+        class Settings:
+            key: Secret[str] | None = option(default=None, converter=empty_str_to_none2)
+
+        @command()
+        @click_options(Settings, [LOADER], show_envvars_in_help=True)
+        def cli(settings: Settings, /) -> None:
+            if (value := settings.key) is None:
+                echo("key = None")
+            else:
+                echo(f"key = {value.get_secret_value()}")
+
+        result = CliRunner().invoke(cli, args=["--key", "value"])
+        assert result.exit_code == 0
+        assert result.stdout == "key = value\n"
+
+    def test_value_via_env(self) -> None:
+        @settings
+        class Settings:
+            key: Secret[str] | None = option(default=None, converter=empty_str_to_none2)
+
+        @command()
+        @click_options(Settings, [LOADER], show_envvars_in_help=True)
+        def cli(settings: Settings, /) -> None:
+            if (value := settings.key) is None:
+                echo("key = None")
+            else:
+                echo(f"key = {value.get_secret_value()}")
+
+        result = CliRunner().invoke(cli, args=["--key", "value"])
+        assert result.exit_code == 0
+        assert result.stdout == "key = value\n"
