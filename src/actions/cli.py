1163c17694e2dbd0f2897bf09eb00067bef593b7
Derek Wan <d.wan@icloud.com>
2025-12-25 08:53:39 +0900

    2025-12-25 08:53:39 (Thu)  > DW-Mac  > derekwan

--- src/actions/cli.py
+++ src/actions/cli.py
@@ -6,6 +6,7 @@ from utilities.click import CONTEXT_SETTINGS
 from actions.hooks.cli import hooks_sub_cmd
 from actions.publish.cli import publish_sub_cmd
 from actions.requirements.cli import requirements_sub_cmd
+from actions.sequences.cli import sequences_sub_cmd
 from actions.sleep.cli import sleep_sub_cmd
 from actions.tag.cli import tag_sub_cmd
 
@@ -17,6 +18,7 @@ def _main() -> None: ...
 _ = _main.command(name="hooks", **CONTEXT_SETTINGS)(hooks_sub_cmd)
 _ = _main.command(name="publish", **CONTEXT_SETTINGS)(publish_sub_cmd)
 _ = _main.command(name="requirements", **CONTEXT_SETTINGS)(requirements_sub_cmd)
+_ = _main.command(name="sequences", **CONTEXT_SETTINGS)(sequences_sub_cmd)
 _ = _main.command(name="sleep", **CONTEXT_SETTINGS)(sleep_sub_cmd)
 _ = _main.command(name="tag", **CONTEXT_SETTINGS)(tag_sub_cmd)
 
