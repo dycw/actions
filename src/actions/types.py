780300791ee989cbb5a12da3b036e52c4514a499
Derek Wan <d.wan@icloud.com>
2026-01-05 09:23:12 +0900

    2026-01-05 09:23:12 (Mon)  > DW-Mac  > derekwan

--- src/actions/types.py
+++ src/actions/types.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+from typing import Any
+
 from typed_settings import Secret
 
 type SecretLike = str | Secret[str]
+type StrDict = dict[str, Any]
 
 
-__all__ = ["SecretLike"]
+__all__ = ["SecretLike", "StrDict"]
