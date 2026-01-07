5c383b1dd021949adc3f6d0d07bb5d34dd6ef23c
Derek Wan <d.wan@icloud.com>
2026-01-07 15:30:28 +0900

    2026-01-07 15:30:28 (Wed)  > DW-Mac  > derekwan

--- src/actions/cli.py
+++ src/actions/cli.py
@@ -3,10 +3,6 @@ from __future__ import annotations
 from click import group
 from utilities.click import CONTEXT_SETTINGS
 
-import actions.publish_package.doc
-import actions.random_sleep.doc
-import actions.run_hooks.doc
-import actions.tag_commit.doc
 from actions.clean_dir.cli import clean_dir_sub_cmd
 from actions.pre_commit.conformalize_repo.cli import conformalize_repo_sub_cmd
 from actions.pre_commit.conformalize_repo.constants import CONFORMALIZE_REPO_SUB_CMD
@@ -19,10 +15,14 @@ from actions.pre_commit.replace_sequence_strs.constants import (
 from actions.pre_commit.touch_empty_py.cli import touch_empty_py_sub_cmd
 from actions.pre_commit.touch_empty_py.constants import TOUCH_EMPTY_PY_SUB_CMD
 from actions.publish_package.cli import publish_package_sub_cmd
+from actions.publish_package.constants import PUBLISH_PACKAGE_DOCSTRING
 from actions.random_sleep.cli import random_sleep_sub_cmd
+from actions.random_sleep.constants import RANDOM_SLEEP_DOCSTRING
 from actions.run_hooks.cli import run_hooks_sub_cmd
+from actions.run_hooks.constants import RUN_HOOKS_DOCSTRING
 from actions.setup_cronjob.cli import setup_cronjob_sub_cmd
 from actions.tag_commit.cli import tag_commit_sub_cmd
+from actions.tag_commit.constants import TAG_COMMIT_DOCSTRING
 
 
 @group(**CONTEXT_SETTINGS)
@@ -33,22 +33,20 @@ _ = _main.command(name="clean-dir", help="Clean a directory", **CONTEXT_SETTINGS
     clean_dir_sub_cmd
 )
 _ = _main.command(
-    name="publish-package",
-    help=actions.publish_package.doc.DOCSTRING,
-    **CONTEXT_SETTINGS,
+    name="publish-package", help=PUBLISH_PACKAGE_DOCSTRING, **CONTEXT_SETTINGS
 )(publish_package_sub_cmd)
-_ = _main.command(
-    name="run-hooks", help=actions.run_hooks.doc.DOCSTRING, **CONTEXT_SETTINGS
-)(run_hooks_sub_cmd)
-_ = _main.command(
-    name="random-sleep", help=actions.random_sleep.doc.DOCSTRING, **CONTEXT_SETTINGS
-)(random_sleep_sub_cmd)
+_ = _main.command(name="run-hooks", help=RUN_HOOKS_DOCSTRING, **CONTEXT_SETTINGS)(
+    run_hooks_sub_cmd
+)
+_ = _main.command(name="random-sleep", help=RANDOM_SLEEP_DOCSTRING, **CONTEXT_SETTINGS)(
+    random_sleep_sub_cmd
+)
 _ = _main.command(name="setup-cronjob", help="Setup a cronjob", **CONTEXT_SETTINGS)(
     setup_cronjob_sub_cmd
 )
-_ = _main.command(
-    name="tag-commit", help=actions.tag_commit.doc.DOCSTRING, **CONTEXT_SETTINGS
-)(tag_commit_sub_cmd)
+_ = _main.command(name="tag-commit", help=TAG_COMMIT_DOCSTRING, **CONTEXT_SETTINGS)(
+    tag_commit_sub_cmd
+)
 
 
 @_main.group(name="pre-commit", help="Pre-commit hooks", **CONTEXT_SETTINGS)
