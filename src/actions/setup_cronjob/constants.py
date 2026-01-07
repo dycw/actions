a4afb9fb16916fa57d6bf2e81c0637a6c04cf8b6
Derek Wan <d.wan@icloud.com>
2026-01-07 14:02:28 +0900

    2026-01-07 14:02:28 (Wed)  > DW-Mac  > derekwan

--- src/actions/setup_cronjob/constants.py
+++ src/actions/setup_cronjob/constants.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from actions.constants import PATH_ACTIONS
 
-PATH_CONFIGS = PATH_ACTIONS / "conformalize_repo/configs"
+PATH_CONFIGS = PATH_ACTIONS / "setup_cronjob/configs"
 
 
 __all__ = ["PATH_CONFIGS"]
